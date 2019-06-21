7fe9dddeefc63a8d376eb178928969a4e2afa39d
hammer <>
2019-06-21 11:11:50 +0300

    admin

---
+++ beakend/app/controllers/api/settings/isCanAppDownload.js
@@ -0,0 +1,9 @@
+
+const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+module.exports = (backendApp, router) => {
+
+    router.get('/isCanAppDownload', [], (req,res,next) => {
+        res.ok(false)
+    });
+};
