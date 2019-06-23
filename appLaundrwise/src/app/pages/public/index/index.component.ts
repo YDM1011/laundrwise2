edd77c7d8e18c2f8b670413f596b01102688d7a9
hammer <>
2019-06-23 12:12:03 +0300

    admin

--- appLaundrwise/src/app/pages/public/index/index.component.ts
+++ appLaundrwise/src/app/pages/public/index/index.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CrudService} from '../../../crud.service';
+import {Post} from "../../admin/post/post";
 
 @Component({
   selector: 'app-index',
@@ -7,14 +8,14 @@ import {CrudService} from '../../../crud.service';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-  public arrayPost;
+  public arrayPost:Post[] = [];
   constructor(
       private crud: CrudService
   ) {
   }
 
   ngOnInit() {
-    this.crud.get('post?query={}&skip=0&limit=3&sort={"data":-1}', ).then((value) => {
+    this.crud.get('post?query={}&skip=0&limit=3&sort={"data":-1}', ).then((value:any) => {
       this.arrayPost = value;
     }).catch(e => {});
   }
