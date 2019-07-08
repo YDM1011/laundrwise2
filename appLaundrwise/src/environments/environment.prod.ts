82f9cf9ee23a0b7c8eb5db8af84136826499b58d
hammer <>
2019-07-08 14:30:34 +0300

    ws

--- appLaundrwise/src/environments/environment.prod.ts
+++ appLaundrwise/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   host: 'https://piligrim-test.top/api/',
   domain: 'https://piligrim-test.top',
-  ws: 'ws://piligrim-test.top:6759'
+  ws: 'wss://piligrim-test.top:6759'
 };
