6c7d1b47050cb4487778a308754b0b619dc15059
hammer <>
2019-07-08 13:53:32 +0300

    ws

---
+++ beakend/app/config/env/production.js
@@ -0,0 +1,32 @@
+const path = require('path'),
+    rootPath = path.normalize(__dirname + '/../..');
+
+module.exports = {
+    root: rootPath,
+    app: {
+        name: 'Escort'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost:27017',
+    jwtSecret: process.env.JWTSECRET || "secret",
+    email: {
+        host: "smtp.mail.yahoo.com",
+        port: 465,
+        secure: true,
+        user: "ydm101194@yahoo.com",
+        message: "Hello from Tasteol",
+        subject: "Laundrwise",
+        pass: "adn45hrf"
+    },
+    site: {
+        sidDomain: "piligrim-test.top",
+        domain: "https://piligrim-test.top",
+        innerDomain: "https://piligrim-test.top",
+        baseUrl: "https://piligrim-test.top",
+        resetPasswordUrl: "/reset_password/",
+        setPasswordUrl: "/reset_password/"
+    },
+    defaultLanguage: "en",
+    linkSecretKey: "secretKey",
+    rootSecret: "xGCIjhiR4Patsdfasdjrehgkejrg"
+};
