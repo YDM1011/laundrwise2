9669193d869abc5862cf28fba138bcd5939ba286
hammer <>
2019-07-29 10:52:30 +0300

    ``

--- appLaundrwise/src/app/components/my-bar-chart/my-bar-chart.component.ts
+++ appLaundrwise/src/app/components/my-bar-chart/my-bar-chart.component.ts
@@ -29,20 +29,20 @@ export class MyBarChartComponent implements OnInit {
             // const query2 = JSON.stringify({'cleanerOwner': this.cleaner._id});
             // this.crud.getNoCache(`basket/count?query=${query2}`).then((v: any) => {
             //   this.pieChartData.push(v.count);
-              const query5 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 1} );
-              this.crud.getNoCache(`basket/count?query=${query5}`).then((v: any) => {
+              const query1 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 1} );
+              this.crud.getNoCache(`basket/count?query=${query1}`).then((v: any) => {
                 this.pieChartData.push(v.count);
-                const query4 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 2} );
-                this.crud.getNoCache(`basket/count?query=${query4}`).then((v: any) => {
-                  this.pieChartData.push(v.count);const query4 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 3} );
-                  this.crud.getNoCache(`basket/count?query=${query4}`).then((v: any) => {
+                const query2 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 2} );
+                this.crud.getNoCache(`basket/count?query=${query2}`).then((v: any) => {
+                  this.pieChartData.push(v.count);
+                  const query3 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 3} );
+                  this.crud.getNoCache(`basket/count?query=${query3}`).then((v: any) => {
                     this.pieChartData.push(v.count);
-                    const query3 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 4} );
-                    this.crud.getNoCache(`basket/count?query=${query3}`).then((v: any) => {
+                    const query4 = JSON.stringify({'cleanerOwner': this.cleaner._id, status: 4} );
+                    this.crud.getNoCache(`basket/count?query=${query4}`).then((v: any) => {
                       this.pieChartData.push(v.count);
-                        this.pieChartData.push(v.count);
-                        const query4 = JSON.stringify({'cleanerOwner': this.user._id, status: 5});
-                        this.crud.getNoCache(`basket/count?query=${query4}`).then((v: any) => {
+                        const query5 = JSON.stringify({'cleanerOwner': this.user._id, status: 5});
+                        this.crud.getNoCache(`basket/count?query=${query5}`).then((v: any) => {
                             this.pieChartData.push(v.count);
                             this.loading = true;
                         });
@@ -53,6 +53,7 @@ export class MyBarChartComponent implements OnInit {
             // });
           });
         }
+
         if (this.user.role === 'superManagerDelivery') {
           const query = JSON.stringify({'superManager': this.user._id});
           this.crud.getNoCache(`delivery?query=${query}`).then((v: any) => {
